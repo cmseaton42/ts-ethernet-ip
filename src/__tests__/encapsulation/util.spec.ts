d53ad27a7539cd111b117fcbe033e951bf2b1a53
Canaan Seaton <cmseaton42@gmail.com>
2019-08-27 19:37:45 -0700

    Add encapsulation utility functions

---
+++ src/__tests__/encapsulation/util.spec.ts
@@ -0,0 +1,35 @@
+import { registerSession, unregisterSession, sendRRData, sendUnitData } from '../../enip/encapsulation/util';
+
+describe('Encapsulation utility functions', () => {
+    describe('Register session utility', () => {
+        it('Generates correct buffer', () => {
+            const data = registerSession();
+
+            expect(data).toMatchSnapshot();
+        });
+    });
+
+    describe('Unregister session utility', () => {
+        it('Generates correct buffer', () => {
+            const data = unregisterSession(98705);
+
+            expect(data).toMatchSnapshot();
+        });
+    });
+
+    describe('SendRRData session utility', () => {
+        it('Generates correct buffer', () => {
+            const data = sendRRData(98705, Buffer.from('hello world'));
+
+            expect(data).toMatchSnapshot();
+        });
+    });
+
+    describe('SendUnitData session utility', () => {
+        it('Generates correct buffer', () => {
+            const data = sendUnitData(98705, Buffer.from('hello world'), 32145, 456);
+
+            expect(data).toMatchSnapshot();
+        });
+    });
+});
--- src/enip/encapsulation/util.ts
+++ src/enip/encapsulation/util.ts
@@ -1,23 +1,87 @@
+import { Header, Commands } from './header';
+import { CPF, TypeIDs } from './common-packet-format';
+
 /**
- * Parses Encapulation Status Code to Human Readable Error Message.
+ * Returns a Register Session Request String
  */
-export const parseStatus = (status: number): string => {
-    switch (status) {
-        case 0x00:
-            return "SUCCESS";
-        case 0x01:
-            return "FAIL: Sender issued an invalid ecapsulation command.";
-        case 0x02:
-            return "FAIL: Insufficient memory resources to handle command.";
-        case 0x03:
-            return "FAIL: Poorly formed or incorrect data in encapsulation packet.";
-        case 0x64:
-            return "FAIL: Originator used an invalid session handle.";
-        case 0x65:
-            return "FAIL: Target received a message of invalid length.";
-        case 0x69:
-            return "FAIL: Unsupported encapsulation protocol revision.";
-        default:
-            return `FAIL: General failure - Error: <${status}> occured.`;
-    }
+export const registerSession = (): Buffer => {
+    const cmdBuf = Buffer.alloc(4);
+    cmdBuf.writeUInt16LE(0x01, 0); // Protocol Version (Required to be 1)
+    cmdBuf.writeUInt16LE(0x00, 2); // Opton Flags (Reserved for Future List)
+
+    // Build Register Session Buffer and return it
+    return Header.build(Commands.REGISTER_SESSION, 0x00, cmdBuf);
 }
+
+/**
+ * Returns an Unregister Session Request String
+ *
+ * @param {number} session - Encapsulation Session ID
+ * @returns {string} unregister seeion strings
+ */
+export const unregisterSession = (session: number): Buffer => {
+    // Build Unregister Session Buffer
+    return Header.build(Commands.UNREGISTER_SESSION, session);
+};
+
+/**
+ * Returns a UCMM Encapsulated Packet String
+ * 
+ * @param {number} [timeout=10] - Timeout (sec)
+ */
+export const sendRRData = (session: number, data: Buffer, timeout: number = 10) => {
+    const timeoutBuf = Buffer.alloc(6);
+    timeoutBuf.writeUInt32LE(0x00, 0); // Interface Handle ID (Shall be 0 for CIP)
+    timeoutBuf.writeUInt16LE(timeout, 4); // Timeout (sec)
+
+    // Enclose in Common Packet Format
+    let buf = CPF.build([
+        { TypeID: TypeIDs.NULL, data: Buffer.from([]) },
+        { TypeID: TypeIDs.UCMM, data }
+    ]);
+
+    // Join Timeout Data with buffer
+    buf = Buffer.concat([timeoutBuf, buf]);
+
+    // Build SendRRData Buffer
+    return Header.build(Commands.SEND_RR_DATA, session, buf);
+};
+
+/**
+ * Returns a Connected Message Datagram (Transport Class 3) String
+ *
+ * @param {number} ConnectionID - Connection ID from FWD_OPEN
+ * @param {number} SequenceNumber - Sequence Number of Datagram
+ */
+export const sendUnitData = (session: number, data: Buffer, ConnectionID: number, SequnceNumber: number): Buffer => {
+    const timeoutBuf = Buffer.alloc(6);
+    timeoutBuf.writeUInt32LE(0x00, 0); // Interface Handle ID (Shall be 0 for CIP)
+    timeoutBuf.writeUInt16LE(0x00, 4); // Timeout (sec) (Shall be 0 for Connected Messages)
+
+    // Enclose in Common Packet Format
+    const seqAddrBuf = Buffer.alloc(4);
+    seqAddrBuf.writeUInt32LE(ConnectionID, 0);
+    const seqNumberBuf = Buffer.alloc(2);
+    seqNumberBuf.writeUInt16LE(SequnceNumber, 0);
+    const ndata = Buffer.concat([
+        seqNumberBuf,
+        data
+    ]);
+
+    let buf = CPF.build([
+        {
+            TypeID: TypeIDs.CONNECTION_BASED,
+            data: seqAddrBuf
+        },
+        {
+            TypeID: TypeIDs.CONNECTED_TRANSPORT_PACKET,
+            data: ndata
+        }
+    ]);
+
+    // Join Timeout Data with
+    buf = Buffer.concat([timeoutBuf, buf]);
+
+    // Build SendRRData Buffer
+    return Header.build(Commands.SEND_UNIT_DATA, session, buf);
+};
